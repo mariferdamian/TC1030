152ad67063a7fd15692b46909841e005e962a879
Maria <mariferdamian0@gmail.com>
2021-05-12 17:26:38 -0700

    Funcion vendedor

--- Inventario.cpp
+++ Inventario.cpp
@@ -3,8 +3,6 @@ using namespace std;
 #include <ctime>
 #include <vector>
 
-
-
 class Vendedor {
      public:
         string cve_vendedor;
@@ -22,7 +20,7 @@ class Inventario {
 
 class Cliente {
         string cve_cliente;
-        string nombrec;  
+        string nombre;  
 };
 
 class Factura{
@@ -41,33 +39,42 @@ string GETDATE(){
     return out;
 }
 
-vector<Factura> generaFactura(int contadorFactura, vector<Factura> arreglof,Vendedor v1, Inventario i2, int cant){
+void generaFactura(int contadorFactura, vector<Factura> arreglof[],vector<Vendedor> v1, vector<Inventario> i2, int cant){
  
     Factura f1;
-   
     f1.cve_vendedor = v1.cve_vendedor;
     f1.cve_articulo = i2.cve_articulo;
     f1.cantidad = cant;
     f1.noFactura = "F" + GETDATE();
-    arreglof.push_back(f1); 
-    return arreglof;
-};
+    arreglof[1] = f1; 
 
+};
+/*
+void agregarVendedor(vector<Vendedor>arreglov[]){
+    string nombre;
+    string cve_vendedor;
+    cout << "Ingresa nombre del vendedor ";
+    cin >> nombre;
+    cout << "Ingresa clave del vendedor ";
+    cin >> cve_vendedor;
+    string v = nombre + ","+ cve_vendedor;
+    arreglov.push_back(v);
+};
+*/
 
 int main(){
     vector<Vendedor> arreglov;
-    vector <Inventario> arregloi;
-    vector<Factura> arreglof;
+    vector<Inventario> arregloi;
+    vector <Factura> arreglof;
     
+
     // variable v1 y v2
     Vendedor v1 = {"v100","Don Julio Estrella"};
-    Vendedor v2 = {"v100","Doña Esperanza Luna"};
+    Vendedor v2 = {"v101","Doña Esperanza Luna"};
     arreglov.push_back(v1);
     arreglov.push_back(v2);
 
-    
     // variable i1 y i2
-    
     Inventario i1 = {"i500","Tornillo sin fin",10.0};
     Inventario i2 = {"i600","Piñón Cremallera",10.0};
     Inventario i3 = {"i700","Angulo de Ackerman",10.0};
@@ -76,24 +83,24 @@ int main(){
     arregloi.push_back(i3);
     
     int contadorFactura = 0;
-    arreglof =generaFactura(contadorFactura, arreglof, v1, i2, 32);
-    
+    generaFactura(contadorFactura, arreglof, v1, i2, 32);
+
     string nombreArticulo;
     // imprimimos todas las facturas
     cout << "Facturas Generadas "<< endl;
-    cout << "No\t\tVendedor\tArticulo\tDescripción"<<endl;
-    
+    cout << "No" <<"\t\t"<<"Vendedor"<<"\t"<<"Articulo"<<endl;
+
     for (int j=0; j<=contadorFactura; j++){
         for (int k=0;k<arregloi.size();k++){
             if (arreglof[j].cve_articulo == arregloi[k].cve_articulo)
                 nombreArticulo = arregloi[k].descripcion;
         }
         cout <<arreglof[j].noFactura<<"\t"
-            <<arreglof[j].cve_vendedor<<"\t\t"
-            <<arreglof[j].cve_articulo<<"\t\t"
+            <<arreglov[j].cve_vendedor<<"\t\t"
+            <<arregloi[j].cve_articulo<<"\t"
             <<nombreArticulo<<endl;
 
     }
-
+    
     return 0;
 };
