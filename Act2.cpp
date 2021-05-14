63a37d72c363ee74259eb9635fea113bb424fcc0
Monty3141 <juanpablomontoyae@gmail.com>
2021-05-13 18:56:23 -0700

    Se añadieron las clases utilizando constructores

--- Act2.cpp
+++ Act2.cpp
@@ -8,7 +8,10 @@ using namespace std;
 class Vendedor {
     public:
      Vendedor();
-     Vendedor(string clave, string nombre);
+     Vendedor(string clave, string nombreVendedor){
+         cve_vendedor=clave;
+         nombre=nombreVendedor;
+     };
      void setVend(string clave, string nombre);
      string getVend(){return cve_vendedor;};
      string getNombre(){return nombre;};
@@ -18,23 +21,53 @@ class Vendedor {
         string nombre;
 
 };
+class Cliente {
+    public:
+     Cliente();
+     Cliente(string clave, string nombreCliente){
+         cve_Cliente=clave;
+         nombre=nombreCliente;
+     };
+     void setCliente(string clave, string nombre);
+     string getCliente(){return cve_Cliente;};
+     string getNombre(){return nombre;};
 
+    private:
+        string cve_Cliente;
+        string nombre;
 
-Vendedor::Vendedor() {
-    cve_vendedor = "vacante";
-    nombre = "nadie";
+};
+class Inventario {
+    public:
+     Inventario();
+     Inventario(string clave, string nombreInventario, string des, double p){
+         cve_Inventario=clave;
+         nombre=nombreInventario;
+         descripcion=des;
+         precio=p;
+     };
+     void setInventario(string clave, string nombre);
+     string getInventario(){return cve_Inventario;};
+     string getNombre(){return nombre;};
+     string getDescripcion(){return descripcion;};
+     double getPrecio(){return precio;};
+        
+    private:
+        string cve_Inventario;
+        string nombre;
+        string descripcion;
+        double precio;
+
 };
 
-Vendedor::Vendedor(string clave, string nombre) {
-    cve_vendedor = clave;
-    nombre = nombre;
-};
+
+
 
 int main(){
-  Vendedor v1;
+  Vendedor v1("hola", "hola");
   Vendedor v2("v100","kiubo");
-
-  cout<<v1.getVend()<<" - "<<v1.getNombre()<<"\n";
+  Inventario c1("il", "il", "de", 50);
+  cout<<c1.getInventario()<<" - "<<c1.getNombre()<<c1.getDescripcion()<<c1.getPrecio()<<"\n";
   cout<<v2.getVend()<<" - "<<v2.getNombre();
   
   return 0;
