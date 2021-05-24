6a3b9362b7e515a4a3d3333bb50c7938cfa93e7f
Monty3141 <juanpablomontoyae@gmail.com>
2021-05-23 18:41:35 -0700

    updated main

--- Tarea1/main.cpp
+++ Tarea1/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <variant>
 #include "Cap.h"
 #include "Thor.h"
 #include "Ironman.h"
@@ -7,23 +9,57 @@
 using namespace std;
 
 int main(){
-    Cap c(1);
-    c.setLevel(10);
-    cout<<c.getTraje()<<endl;
-    c.run();
-    c.jump();
-    c.lanzarEscudo();
-    Thor t(1);
-    t.setLevel(10);
-    cout<<t.getTraje()<<endl;
-    t.run();
-    t.jump();
-    t.lanzarMartillo();
-    Ironman I(1);
-    I.setLevel(10);
-    cout<<I.getTraje()<<endl;
-    I.run();
-    I.jump();
-    I.volar();
+    
+    vector<Cap> capitanes;
+    vector<Thor> thors;
+    vector<Ironman> ironmans;
+    bool finish=1;
+    cout<<"Agregar capitanes"<<endl;
+    while (finish){
+        capitanes.push_back(Cap(1));
+        cout<<"Do you want to continue"<<endl;
+        cin>>finish;
+
+    }
+    finish=1;
+    cout<<"Agregar thores"<<endl;
+    while (finish){
+        thors.push_back(Thor(1));
+        cout<<"Do you want to continue"<<endl;
+        cin>>finish;
+    }
+    finish=1;
+    cout<<"Agregar ironmans"<<endl;
+    while(finish){
+        
+        ironmans.push_back(Ironman(1));
+        cout<<"Do you want to continue"<<endl;
+        cin>>finish;
+    }
+    for (int i =0; i< capitanes.size(); i++){
+        cout<<"Cap "<<i<<endl;
+        cout<<"Level"<<capitanes[i].getLevel()<<endl;
+        capitanes[i].jump();
+        capitanes[i].run();
+        cout<<"Suit"<<capitanes[i].getTraje()<<endl;
+        capitanes[i].lanzarEscudo();
+    }
+    
+    for (int i =0; i< thors.size(); i++){
+        cout<<"Thor "<<i<<endl;
+        cout<<"Level"<<thors[i].getLevel()<<endl;
+        thors[i].jump();
+        thors[i].run();
+        cout<<"Suit"<<thors[i].getTraje()<<endl;
+        thors[i].lanzarMartillo();
+    }
+       for (int i =0; i< ironmans.size(); i++){
+        cout<<"Ironman "<<i<<endl;
+        cout<<"Level"<<ironmans[i].getLevel()<<endl;
+        ironmans[i].jump();
+        ironmans[i].run();
+        cout<<"Suit"<<ironmans[i].getTraje()<<endl;
+        ironmans[i].volar();
+    }
     return 0;
 }
